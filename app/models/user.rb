4e90fb96dd982b131ae3ee3784fb674431f1f419
Rebecca Vo <beckvo@gmail.com>
2018-03-22 15:37:49 +1100

    Minor bug fix and steps 2 - 5 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,9 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  def will_save_change_to_email?
+    false
+  end
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
