ef9489e479ca04a8da9a603d2b1be745d826ff65
Rebecca Vo <beckvo@gmail.com>
2018-03-23 16:38:34 +1100

    Add scenario for test

--- spec/features/creating_books_spec.rb
+++ spec/features/creating_books_spec.rb
@@ -10,8 +10,16 @@ RSpec.feature 'Users can create new books' do
 		fill_in 'Title', with: book.title
 		fill_in 'Author', with: book.author
 		click_button 'Create Book'
-
 		expect(page).to have_current_path(book_path(new_book))
 	end
+
+	scenario 'cannot create book when title is taken' do
+	  fill_in 'Title', with: book.title
+	  fill_in 'TItle', with: book.title
+	  book.save
+	  click_button 'Create Book'
+	  expect(page).to have_content('Title is already taken'.)
+	end
+
 end
 
