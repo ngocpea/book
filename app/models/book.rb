c9236ca1403e8b8963ffe126d0197df817a3a190
Rebecca Vo <beckvo@gmail.com>
2018-03-23 16:26:34 +1100

    Display error message for duplicate title

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,13 @@
 class Book
   include Mongoid::Document
 
-
   field :title, type: String
   field :author, type: String
   field :slug, type: String
 
   belongs_to :user, optional: true
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :author, presence: true
 
 end
