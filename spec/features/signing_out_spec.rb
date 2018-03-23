333cbb636766b45de673dfd829837dffe3b2a7ee
Rebecca Vo <beckvo@gmail.com>
2018-03-23 18:39:16 +1100

    Fix signing_out_spec test

--- spec/features/signing_out_spec.rb
+++ spec/features/signing_out_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'Signed-in users can sign out' do
-	let!(:user) { FactoryBot.create(:user) }
-	
-	before do
-		login_as(user)
-	end
+  let!(:user) { FactoryBot.create(:user) }
 
-	scenario do
-		visit '/'
-		click_link 'Sign out'
-		#expect(page).to have_content "Signed out successfully."
-	end
+  before do
+    login_as(user)
+  end
+
+  scenario 'when logged in' do
+	  visit '/'
+	  click_link 'Sign out'
+	  expect(page).to have_link('Sign up')
+    end
 end
