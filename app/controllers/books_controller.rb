a2311163374bd356e8b77fa49fd0e8be880d0fd9
Rebecca Vo <beckvo@gmail.com>
2018-03-23 19:38:40 +1100

    Implement delete action for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,6 +39,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
 private
 
   # def book_owner?
